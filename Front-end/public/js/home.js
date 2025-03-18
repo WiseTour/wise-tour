789dcbda629f4b2816ca5287938f5b005528a4ca
sardinh4 <leonardo_sardinha@outlook.com>
2025-03-18 19:28:39 -0300

    padronizando headers

--- Front-end/public/js/home.js
+++ Front-end/public/js/home.js
@@ -1,11 +1,4 @@
-window.addEventListener("scroll", function () {
-    const header = document.getElementById("header");
-    if (window.scrollY > 50) {
-        header.style.backgroundColor = "#F8CA26";
-    } else {
-        header.style.backgroundColor = "transparent";
-    }
-});
+
 
 function mobileNav() {
     var x = document.getElementById("header-container-mobile");
