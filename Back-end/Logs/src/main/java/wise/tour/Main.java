9f5383f42b0b1a672a20e6f4535cff0f0aabb98d
Phelipe <phelipe.silva@sptech.school>
2025-04-06 23:41:23 -0300

    Adicionando projeto Log v2.0

--- Back-end/Logs/src/main/java/wise/tour/Main.java
+++ Back-end/Logs/src/main/java/wise/tour/Main.java
@@ -2,6 +2,6 @@ package wise.tour;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("-------------- LOGS V2.0 WISE TOUR --------------");
+        System.out.println("-------------- LOGS V2.0 WISE TOUR ---------------");
     }
 }
