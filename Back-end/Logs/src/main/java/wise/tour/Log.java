9ec892d2e5f360d26d84b54d70526e113ed866fa
Phelipe <phelipe.silva@sptech.school>
2025-04-07 23:58:13 -0300

    Modificando classes Main e Log

--- Back-end/Logs/src/main/java/wise/tour/Log.java
+++ Back-end/Logs/src/main/java/wise/tour/Log.java
@@ -1,5 +1,35 @@
 package wise.tour;
 
-public class Log {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
+public class Log {
+    private static final Logger logger = LoggerFactory.getLogger(Log.class);
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+    public void showLog(String msg, String msgType)
+    {
+        String dataHora = LocalDateTime.now().format(formatter);
+        String mensagemFormatada = dataHora + " - " + msg;
+
+        switch (msgType.toUpperCase())
+        {
+            case "INFO":
+                logger.info(mensagemFormatada);
+                break;
+            case "WARNING":
+                logger.warn(mensagemFormatada);
+                break;
+            case "ERROR":
+                logger.error(mensagemFormatada);
+                break;
+            case "DEBUG":
+                logger.debug(mensagemFormatada);
+                break;
+            default:
+                logger.info(dataHora + "UNKNOWN: " + msg);
+                break;
+        }
+    }
 }
