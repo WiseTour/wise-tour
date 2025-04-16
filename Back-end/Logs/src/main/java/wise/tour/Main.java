c69f348dc8d28edd3e8cadcc9725119aaa6ef8eb
Phelipe <phelipe.silva@sptech.school>
2025-04-15 21:58:04 -0300

    Versão log genérica armazenando no Banco local

--- Back-end/Logs/src/main/java/wise/tour/Main.java
+++ Back-end/Logs/src/main/java/wise/tour/Main.java
@@ -1,30 +1,38 @@
 package wise.tour;
 
+import java.time.format.DateTimeFormatter;
 import java.util.Random;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        System.out.println("-------------- LOGS V2.0 WISE TOUR ---------------");
-        Log log = new Log(); // Instanciando o objeto da classe Log
-        Random random = new Random(); // Instanciando o objeto para gerar números aleatórios
+    public static void main(String[] args) {
+      Log log = new Log();
+      Random random = new Random();
+      DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-        log.showLog("Iniciando o processo ETL...", "INFO");
+      try
+      {
+        log.showLog("Iniciando o processo ETL...", "INFO", "Início", formatter);
         Thread.sleep((random.nextInt(5) + 1) * 1000L);
 
-        log.showLog("Extraindo dados...", "INFO");
+        log.showLog("Extraindo dados...", "INFO", "Extração", formatter);
         Thread.sleep((random.nextInt(5) + 1) * 1000L);
 
-        log.showLog("Transformando dados...", "INFO");
+        log.showLog("Transformando dados...", "INFO", "Transformação", formatter);
         Thread.sleep((random.nextInt(5) + 1) * 1000L);
 
-        log.showLog("Poucos dados extraídos, alguns dados podem estar faltando. Verifique a origem.", "WARNING");
+        log.showLog("Poucos dados extraídos, alguns dados podem estar faltando. Verifique a origem.", "WARNING", "Extração", formatter);
         Thread.sleep((random.nextInt(5) + 1) * 1000L);
 
-        log.showLog("Carregando dados no banco...", "INFO");
+        log.showLog("Carregando dados no banco...", "INFO", "Carga", formatter);
         Thread.sleep((random.nextInt(5) + 1) * 1000L);
 
-        log.showLog("Falha ao carregar os dados. Banco de dados não encontrado.", "ERROR");
+        log.showLog("Falha ao carregar os dados. Banco de dados não encontrado.", "ERROR", "Carga", formatter);
 
-        log.showLog("Processo ETL encerrado!", "INFO");
+        log.showLog("Processo ETL encerrado!", "INFO", "Fim", formatter);
+      } catch (InterruptedException e)
+      {
+        System.out.println("ERRO!");
+      }
+
     }
 }
